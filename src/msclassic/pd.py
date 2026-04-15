7a82ff367462f03ce360f7975863a359ceb017ca
dogtopus <dogtopus@users.noreply.github.com>
2026-04-16 00:38:30 +0900

    Working MS classic decoder

--- src/msclassic/pd.py
+++ src/msclassic/pd.py
@@ -1,6 +1,7 @@
+from dataclasses import dataclass
 from enum import IntEnum, auto
 from itertools import chain, zip_longest
-from typing import TYPE_CHECKING, NoReturn, Optional, Sequence, Tuple
+from typing import TYPE_CHECKING, ClassVar, Dict, List, Literal, NoReturn, Optional, Self, Sequence, Tuple, Type
 from typedsigrokdecode import (
     OUTPUT_ANN,
     AnnotationStream,
@@ -12,14 +13,17 @@ from typedsigrokdecode import (
     OptionMap,
     StackedDecoder,
 )
-
-from msif.stacked import PacketType, RegisterCommon, Tpc, TransactionPacket
+from msif.stacked import CommonRegAccessFormat, IntRegAccessFormat, PacketType, RegAccessFormat, RegisterCommon, Tpc, TransactionPacket
 
 
 class Annotation(IntEnum):
     SET_REG = 0
+    SET_CMD = auto()
     REG_IO = auto()
+    GET_INT = auto()
+    DATA_IO = auto()
     W_LENGTH = auto()
+    W_SEQ = auto()
 
     def name_as_id(self) -> str:
         return self.name.lower().replace("_", "-")
@@ -52,6 +56,133 @@ class RegisterMsClassic(IntEnum):
 
 ALL_REG_NAMES = {i.value: i.name for i in chain(RegisterCommon, RegisterMsClassic)}
 
+
+class AccessType(IntEnum):
+    BLOCK = 0x00
+    PAGE = 0x20
+    OOB = 0x40
+    OOB_NO_ECC = 0x80
+
+
+class Command(IntEnum):
+    READ = 0xaa
+    PROGRAM = 0x55
+    ERASE = 0x99
+    STOP = 0x33
+    RESET = 0x3c
+
+
+# This is a common register but the meaning differs across different device
+# types (for MS classic, bit 3 needs to be set to enable the 4-bit mode, while
+# for MS pro bit 7 needs to be cleared instead).
+class CfgRegAccessFormat(RegAccessFormat):
+    @classmethod
+    def list_names(cls, dir_: Literal['READ', 'WRITE']) -> List[str]:
+        return [f'{cls.RW_GETSET[dir_]} CFG', cls.RW_ABBR[dir_]]
+
+    @classmethod
+    def list_vals(cls, dir_: Literal['READ', 'WRITE'], vals: bytes) -> List[str]:
+        name: str = ''
+
+        if vals[0] & 0x88 == 0x88:
+            name = 'MODE_4BIT'
+        elif vals[0] & 0x80 == 0x80:
+            name = 'MODE_1BIT'
+        else:
+            name = 'UNK'
+
+        return [f'{cls.RW_GETSET[dir_]} CFG = {name} (0x{vals[0]:02X})', cls.REG_RW_ABBR[dir_]]
+
+
+class NandIoRegAccessFormat(RegAccessFormat):
+    ACCESSTYP: ClassVar[Dict[int, str]] = {
+        0x00: 'BLOCK',
+        0x20: 'PAGE',
+        0x40: 'OOB',
+        0x80: 'OOB_NOECC',
+    }
+
+    @classmethod
+    def list_names(cls, dir_: Literal['READ', 'WRITE']) -> List[str]:
+        return [f'{dir_} NANDIO', cls.RW_ABBR[dir_]]
+
+    @classmethod
+    def list_vals(cls, dir_: Literal['READ', 'WRITE'], vals: bytes) -> List[str]:
+        blk = (vals[0] << 16) | (vals[1] << 8) | vals[2]
+        accesstyp = cls.ACCESSTYP.get(vals[3], hex(vals[3]))
+        return [f'{dir_} NANDIO {{ BLK = {blk}, PAGE = {vals[4]}, ACCESSTYP = {accesstyp} }}', cls.REG_RW_ABBR[dir_]]
+
+
+class OobRegAccessFormat(RegAccessFormat):
+    @classmethod
+    def list_names(cls, dir_: Literal['READ', 'WRITE']) -> List[str]:
+        return [f'{cls.RW_GETSET[dir_]} OOB', cls.RW_ABBR[dir_]]
+
+    @classmethod
+    def list_vals(cls, dir_: Literal['READ', 'WRITE'], vals: bytes) -> List[str]:
+        oob = (vals[0] << 24) | (vals[1] << 16) | (vals[2] << 8) | vals[3]
+        # TODO decode manageFlag and overwriFlag and include them in the first label
+        return [f'{dir_} OOB = {oob:08X}', cls.REG_RW_ABBR[dir_]]
+
+
+REG_ACCESS_FORMAT: Dict[Tuple[int, int], Type[RegAccessFormat]] = {
+    (RegisterCommon.STA0, 6): CommonRegAccessFormat,
+    (RegisterCommon.CFG, 1): CfgRegAccessFormat,
+    (RegisterMsClassic.BLK_HI, 5): NandIoRegAccessFormat,
+    (RegisterMsClassic.OOB0, 4): OobRegAccessFormat,
+}
+
+
+def reg_list_names(base: int, size: int, dir_: Literal['READ', 'WRITE']) -> List[str]:
+    formatter = REG_ACCESS_FORMAT.get((base, size))
+    if formatter is None:
+        rr_str = []
+        rr_reg_list = ', '.join(ALL_REG_NAMES.get(i, f'0x{i:02X}') for i in range(base, base + size))
+        rr_str.append(f'{dir_} {{ {rr_reg_list} }}')
+        rr_str.append(f'{dir_} {{ ... }}')
+        rr_str.append(RegAccessFormat.RW_ABBR[dir_])
+        return rr_str
+
+    return formatter.list_names(dir_)
+
+
+def reg_list_vals(base: int, size: int, dir_: Literal['READ', 'WRITE'], vals: bytes) -> List[str]:
+    formatter = REG_ACCESS_FORMAT.get((base, size))
+    if formatter is None:
+        rr_rv_list = ', '.join(
+            f'{ALL_REG_NAMES.get(a, f'0x{a:02X}')} = 0x{v:02X}'
+                for a, v in zip(range(base, base + size), vals)
+        )
+        return [
+            f'{dir_} {{ {rr_rv_list} }}',
+            f'{dir_} {{ ... }}',
+            RegAccessFormat.REG_RW_ABBR[dir_]
+        ]
+    
+    return formatter.list_vals(dir_, vals)
+
+
+@dataclass
+class RwContext:
+    dir_: Optional[Literal['READ', 'WRITE']]
+    block: int
+    page: int
+    access_type: AccessType
+
+    @classmethod
+    def from_packet_data(cls, data: bytes) -> Optional[Self]:
+        blk = (data[0] << 16) | (data[1] << 8) | data[2]
+        if data[3] not in AccessType:
+            return None
+        access_type = AccessType(data[3])
+        page = data[4]
+
+        return cls(dir_=None, block=blk, page=page, access_type=access_type)
+
+    def __str__(self) -> str:
+        return f'{self.dir_ + ' ' if self.dir_ is not None else ''}BLK {self.block}, PAGE {self.page}'
+
+
 class Decoder(
     StackedDecoder[PacketType, NoReturn],
     HasAnnotations,
@@ -70,8 +201,12 @@ class Decoder(
     annotations = format_annotations(
         [
             (Annotation.SET_REG, 'Set Register'),
+            (Annotation.SET_CMD, 'Command'),
             (Annotation.REG_IO, 'Register Access'),
+            (Annotation.GET_INT, 'Card Status'),
+            (Annotation.DATA_IO, 'Data'),
             (Annotation.W_LENGTH, 'Data Length Mismatch'),
+            (Annotation.W_SEQ, 'TPC/Command Sequence Error'),
         ]
     )
 
@@ -81,7 +216,10 @@ class Decoder(
             'Data',
             (
                 Annotation.SET_REG,
+                Annotation.SET_CMD,
                 Annotation.REG_IO,
+                Annotation.GET_INT,
+                Annotation.DATA_IO,
             )
         ),
         (
@@ -89,6 +227,7 @@ class Decoder(
             'Warnings',
             (
                 Annotation.W_LENGTH,
+                Annotation.W_SEQ,
             ),
         ),
     )
@@ -98,11 +237,14 @@ class Decoder(
     rrw: Optional[TransactionPacket] = None
     'Register Read/Write address and byte count'
 
+    rd: Optional[RwContext] = None
+
     def start(self) -> None:
         self.out_ann = self.register(OUTPUT_ANN)
 
     def reset(self) -> None:
         self.rrw = None
+        self.rd = None
 
     def decode(self, start_sample: int, end_sample: int, data: PacketType, /) -> None:
         t, packet = data
@@ -112,8 +254,24 @@ class Decoder(
         if packet.tpc == Tpc.SET_REGS_WINDOW:
             self.annotate_reg_rw(start_sample, end_sample, packet)
             self.rrw = packet
-        if packet.tpc == Tpc.REGS_READ or packet.tpc == Tpc.REGS_WRITE:
+        elif packet.tpc == Tpc.REGS_READ or packet.tpc == Tpc.REGS_WRITE:
             self.annotate_reg_io(start_sample, end_sample, packet)
+            if self.rrw is None:
+                self.put(start_sample, end_sample, self.out_ann, (
+                    Annotation.W_SEQ,
+                    ['No prior SET_REGS_WINDOW before register access.'],
+                ))
+                return
+            _, _, wa, wb = self.rrw.data
+            # TODO: What about fragmented acess?
+            if packet.tpc == Tpc.REGS_WRITE and wa == RegisterMsClassic.BLK_HI and wb == 5:
+                self.rd = RwContext.from_packet_data(packet.data)
+        elif packet.tpc == Tpc.GET_INT:
+            self.annotate_int(start_sample, end_sample, packet)
+        elif packet.tpc == Tpc.READ_LONG_DATA or packet.tpc == Tpc.WRITE_LONG_DATA:
+            self.annotate_data(start_sample, end_sample, packet)
+        elif packet.tpc == Tpc.SET_CMD:
+            self.annotate_cmd(start_sample, end_sample, packet)
 
     def annotate_reg_rw(self, start_sample: int, end_sample: int, packet: TransactionPacket) -> None:
         if len(packet.data) != 4:
@@ -126,15 +284,9 @@ class Decoder(
         # TODO name common register access patterns (OOB, NANDIO, COMMON, CFG, etc.)
 
         if rb != 0:
-            rr_reg_list = ', '.join(ALL_REG_NAMES.get(i, f'0x{i:02X}') for i in range(ra, ra + rb))
-            rr_str.append(f'READ {{ {rr_reg_list} }}')
-            rr_str.append('READ { ... }')
-            rr_str.append('R')
+            rr_str.extend(reg_list_names(ra, rb, 'READ'))
         if wb != 0:
-            rw_reg_list = ' '.join(ALL_REG_NAMES.get(i, f'0x{i:02X}') for i in range(wa, wa + wb))
-            rw_str.append(f'WRITE {{ {rw_reg_list} }}')
-            rw_str.append('WRITE { ... }')
-            rw_str.append('W')
+            rw_str.extend(reg_list_names(wa, wb, 'WRITE'))
 
         self.put(
             start_sample,
@@ -142,7 +294,7 @@ class Decoder(
             self.out_ann, (
                 Annotation.SET_REG,
                 [
-                    *(' '.join(rw) for rw in zip_longest(rr_str, rw_str, fillvalue='')),
+                    *(' '.join(rwnn for rwnn in rw if rwnn is not None) for rw in zip_longest(rr_str, rw_str)),
                     'S'
                 ]
             )
@@ -157,7 +309,7 @@ class Decoder(
         
         ioa: int
         iob: int
-        name: str
+        name: Literal['READ', 'WRITE']
 
         if tpc == Tpc.REGS_READ:
             ioa, iob = self.rrw.data[0:2]
@@ -172,7 +324,7 @@ class Decoder(
             self.put(start_sample, end_sample, self.out_ann, (Annotation.W_LENGTH, [f'Inconsistent {tpc.name} length.']))
             return
 
-        rr_rv_list = ', '.join(f'{ALL_REG_NAMES.get(a, f'0x{a:02X}')} = 0x{v:02X}' for a, v in zip(range(ioa, ioa + iob), packet.data))
+        rv_str = reg_list_vals(ioa, iob, name, packet.data)
 
         # TODO generate pretty print for common register access patterns (like "READ OOB = C0FBFFFF" or READ NANDIO {BLK = 1, PAGE = 0, ACCESSTYP = 0x20})
 
@@ -181,13 +333,61 @@ class Decoder(
             end_sample,
             self.out_ann, (
                 Annotation.REG_IO,
-                [
-                    f'{name} {{ {rr_rv_list} }}',
-                    f'{name} {{ ... }}',
-                    'RR' if tpc == Tpc.REGS_READ else 'RW'
-                ],
+                rv_str,
             )
         )
+    
+    def annotate_int(self, start_sample: int, end_sample: int, packet: TransactionPacket) -> None:
+        labels = IntRegAccessFormat.list_int(packet.data)
+        self.put(start_sample, end_sample, self.out_ann, (
+            Annotation.GET_INT,
+            labels,
+        ))
+    
+    def annotate_cmd(self, start_sample: int, end_sample: int, packet: TransactionPacket) -> None:
+        cmd = packet.data[0]
+        is_data_access = cmd in (Command.READ, Command.PROGRAM)
+        cmd_str = Command(cmd).name if cmd in Command else f'0x{cmd:02X}'
+
+        self.put(start_sample, end_sample, self.out_ann, (
+            Annotation.SET_CMD,
+            [f'CMD {cmd_str}', 'C']
+        ))
+
+        if self.rd is None and is_data_access:
+            self.put(start_sample, end_sample, self.out_ann, (
+                Annotation.W_SEQ,
+                ['Data access without prior conditions.'],
+            ))
+            return
+
+        if self.rd is not None and is_data_access:
+            self.rd.dir_ = 'READ' if cmd == Command.READ else 'WRITE'
+
+    def annotate_data(self, start_sample: int, end_sample: int, packet: TransactionPacket) -> None:
+        if self.rd is None:
+            self.put(start_sample, end_sample, self.out_ann, (
+                Annotation.W_SEQ,
+                ['Data access without prior conditions.'],
+            ))
+            return
+
+        dir_ = 'READ' if packet.tpc == Tpc.READ_LONG_DATA else 'WRITE'
+        if self.rd.dir_ != dir_:
+            self.put(start_sample, end_sample, self.out_ann, (
+                Annotation.W_SEQ,
+                ['Data access mode is not consistent with previous observation.'],
+            ))
+            return
+
+        hexdump = ' '.join(f'{b:02X}' for b in packet.data)
+
+        self.put(start_sample, end_sample, self.out_ann, (
+            Annotation.DATA_IO, [f'{str(self.rd)} [ {hexdump} ]', 'D'],
+        ))
+
+        if self.rd.access_type == AccessType.BLOCK:
+            self.rd.page += 1
 
 
 if TYPE_CHECKING:
--- src/msif/stacked.py
+++ src/msif/stacked.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, Literal, Tuple, TypeAlias
+from typing import ClassVar, NamedTuple, Dict, List, Literal, Optional, Sequence, Tuple, TypeAlias
 from enum import IntEnum, auto
 
 
@@ -46,3 +46,122 @@ class TransactionPacket(NamedTuple):
 
 PacketType: TypeAlias = Tuple[Literal['txn'], TransactionPacket]
 'All supported packet type.'
+
+
+class RegAccessFormat:
+    RW_ABBR: ClassVar[Dict[Literal['READ', 'WRITE'], str]] = {
+        'READ': 'R',
+        'WRITE': 'W'
+    }
+    REG_RW_ABBR: ClassVar[Dict[Literal['READ', 'WRITE'], str]] = {
+        'READ': 'RR',
+        'WRITE': 'RW'
+    }
+    RW_GETSET: ClassVar[Dict[Literal['READ', 'WRITE'], str]] = {
+        'READ': 'GET',
+        'WRITE': 'SET'
+    }
+
+    @classmethod
+    def list_names(cls, dir_: Literal['READ', 'WRITE']) -> List[str]:
+        raise NotImplementedError()
+
+    @classmethod
+    def list_vals(cls, dir_: Literal['READ', 'WRITE'], vals: bytes) -> List[str]:
+        raise NotImplementedError()
+
+
+# TODO separate the fields into their own format classes and optionally call
+# them here so we won't lose bit decoding of the registers when they get
+# accessed individually.
+class CommonRegAccessFormat(RegAccessFormat):
+    # ECC_FAIL_* means an ECC correction is attempted but failed (more than
+    # 1 bit has been corrupted), and ECC_CRCT_* means a single-bit error has
+    # been corrected by ECC.
+    STA_FLAGS: ClassVar[Sequence[Tuple[int, str]]] = [
+        (1 << 0, 'ECC_FAIL_CONFIG'),
+        (1 << 1, 'ECC_CRCT_CONFIG'),
+        (1 << 2, 'ECC_FAIL_OOB'),
+        (1 << 3, 'ECC_CRCT_OOB'),
+        (1 << 4, 'ECC_FAIL_DATA'),
+        (1 << 5, 'ECC_CRCT_DATA'),
+        (1 << 8, 'WR_PROT'),
+        (1 << 9, 'SLEEP'),
+    ]
+
+    # The memo says that 0xff is MS classic, however none of my MS classic
+    # cards have this set to 0xff, instead they all have this set to 0x00,
+    # so we assume here that maybe 0xff is some reserved value that was seen
+    # on very old MS classic cards.
+    TYPES: ClassVar[Dict[int, str]] = {
+        0x00: 'MS_CLASSIC',
+        0x01: 'MS_PRO',
+        0xff: 'UNSET',
+    }
+
+    CATEGORIES: ClassVar[Dict[int, str]] = {
+        0x00: 'DEFAULT',
+        0xff: 'UNSET',
+    }
+
+    CLASSES: ClassVar[Dict[int, str]] = {
+        0x00: 'DEFAULT',
+        0x01: 'ROM',
+        0xff: 'UNSET',
+    }
+
+    @classmethod
+    def list_names(cls, dir_: Literal['READ', 'WRITE']) -> List[str]:
+        return [f'{cls.RW_GETSET[dir_]} COMMON', cls.RW_ABBR[dir_]]
+
+    @classmethod
+    def list_vals(cls, dir_: Literal['READ', 'WRITE'], vals: bytes) -> List[str]:
+        sta0, sta1, type_, unk_0x5, category, class_ = vals
+        sta = (sta0 << 8) | sta1
+        sta_names = list(name for flag, name in cls.STA_FLAGS if flag & sta == flag)
+        sta_str = (' {' + ', '.join(sta_names) + '}') if len(sta_names) != 0 else ''
+
+        type_str = cls.TYPES.get(type_, f'0x{type_:02X}')
+
+        category_str = cls.CATEGORIES.get(category, f'0x{type_:02X}')
+
+        class_str = cls.CLASSES.get(class_, f'0x{class_:02X}')
+
+        return [
+            f'{cls.RW_GETSET[dir_]} COMMON {{ STA = 0x{sta:02X}{sta_str}, TYPE = {type_str}, 0x05 = 0x{unk_0x5:02X}, CATEGORY = {category_str}, CLASS = {class_str} }}',
+            f'{cls.RW_GETSET[dir_]} COMMON {{ ... }}',
+            cls.REG_RW_ABBR[dir_]
+        ]
+
+
+# Doubles as a decoder for GET_INT. Use list_int() for such purpose.
+class IntRegAccessFormat(RegAccessFormat):
+    INT_FLAGS: ClassVar[Sequence[Tuple[int, str]]] = [
+        (1 << 0, 'UNK_CMD'),
+        (1 << 5, 'REQ_DATA'),
+        (1 << 6, 'ERR'),
+        (1 << 7, 'DONE'),
+    ]
+
+    @classmethod
+    def list_names(cls, dir_: Literal['READ', 'WRITE']) -> List[str]:
+        return [f'{cls.RW_GETSET[dir_]} INT', cls.RW_ABBR[dir_]]
+
+    @classmethod
+    def _list_vals(cls, dir_: Literal['READ', 'WRITE'], vals: bytes, abbr: Optional[str] = None) -> List[str]:
+        int_ = vals[0]
+        int_names = list(name for flag, name in cls.INT_FLAGS if flag & int_ == flag)
+        int_str = (' { ' + ', '.join(int_names) + ' }') if len(int_names) != 0 else ''
+
+        return [
+            f'{cls.RW_GETSET[dir_]} INT = 0x{int_:02X}{int_str}',
+            cls.REG_RW_ABBR[dir_] if abbr is None else abbr
+        ]
+
+    @classmethod
+    def list_vals(cls, dir_: Literal['READ', 'WRITE'], vals: bytes) -> List[str]:
+        return cls._list_vals(dir_, vals)
+
+    @classmethod
+    def list_int(cls, vals: bytes) -> List[str]:
+        return cls._list_vals('READ', vals, abbr='I')
